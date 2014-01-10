352329c9b5d1751b6746e6570183c095e95cc6b1
Eldon Nelson <eldon_nelson@ieee.org>
2014-01-09 22:25:42 -0600

    make code not merge instances

--- covmerge/covmerge4.sv
+++ covmerge/covmerge4.sv
@@ -2,9 +2,9 @@ module dut (input bit x, input bit clk);
 
     covergroup c1_cg (ref bit x);
         
-        option.per_instance = 1;
-        option.get_inst_coverage = 1;
-        type_option.merge_instances = 1;
+        option.per_instance = 0;
+        option.get_inst_coverage = 0;
+        type_option.merge_instances = 0;
         
         x : coverpoint x {
             bins _0 = {1'h0};      
